b789e725af9d4cfb3fb3d0818e8b50a1f5086f95
mdhralif <hasiburrahman999.alif@gmail.com>
2025-07-13 01:08:05 +0600

    .

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,21 +1,11 @@
-// import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
-
-// const isProtectedRoute = createRouteMatcher(["/settings(.*)","/"]);
-
-// export default clerkMiddleware((auth, req) => {
-//   if (isProtectedRoute(req)) auth().protect();
-// });
-
-// export const config = {
-//   matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
-// };
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-const isProtectedRoute = createRouteMatcher(["/", "/credits(.*)"]);
+const isProtectedRoute = createRouteMatcher(["/settings(.*)","/"]);
+
 export default clerkMiddleware((auth, req) => {
-if (isProtectedRoute(req)) {
-auth().protect();
-}
+  if (isProtectedRoute(req)) auth().protect();
 });
 
-export const config = { matcher: ["/((?!...|_next).)", "/", "/(api|trpc)(.)"] };
+export const config = {
+  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
+};
