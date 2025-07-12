521ff0e9ad7b42918980f18bbe04d480805ce067
mdhralif <hasiburrahman999.alif@gmail.com>
2025-07-13 00:27:48 +0600

    .

--- src/app/sign-in/[[...sign-in]]/page.tsx
+++ src/app/sign-in/[[...sign-in]]/page.tsx
@@ -1,10 +1,12 @@
+'use client';
+
 import { SignIn } from "@clerk/nextjs";
 
 export default function Page() {
   // Debug log to check environment variables
   console.log("NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY:", process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY);
   console.log("NEXT_PUBLIC_CLERK_SIGN_IN_URL:", process.env.NEXT_PUBLIC_CLERK_SIGN_IN_URL);
-  
+
   return (
     <div className="h-[calc(100vh-96px)] flex items-center justify-center">
       <SignIn />
